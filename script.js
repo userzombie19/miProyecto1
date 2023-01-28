1f8076a47d03a500ba6cbcf92a8797c44d6ab549
userzombie19 <122949813+userzombie19@users.noreply.github.com>
2023-01-28 18:17:42 -0300

    este es mi primer commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function evento1() {
+    alert("hola")
+    console.log("hola estoy aprendiendo javascript")
+}       /*muestra mensaje que dice hola al hacer doble click en la imagen
+y tambien envia un mensaje a la consola */ 
